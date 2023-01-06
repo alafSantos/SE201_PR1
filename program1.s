968ba99c3c901cc9ddc60c9e55597d5238835370
Alessandro Mandrile <mandrile@feedbackitalia.it>
2023-01-06 09:14:32 +0100

    ~fix labels and registers order

--- program1.s
+++ program1.s
@@ -2,9 +2,9 @@
     addi    a7,a0,0x00
     addi    a0,a3,0x00
     beq     a7,zero, a0_eq_0 #[0x40 + PC] #0x48
-    beq     a1,zero, a1_eq_0 #0x50
-    beq     a2,zero, a2_eq_0 #0x50
-    bge     a3,zero, a3_le_0
+    beq     a1,zero, a1a2_eq_0 #0x50
+    beq     a2,zero, a1a2_eq_0 #0x50
+    bge     zero,a3, a3_le_0
     addi    a5,a7,0x00
     slli    a4,a3,0x02
     add     a7,a7,a4
@@ -16,13 +16,12 @@ if_5:
     addi    a5,a5,0x04
     addi    a1,a1,0x04
     addi    a2,a2,0x04
-a2_eq_0:
     bne     a5,a7, if_5 #0x24
     ret     #jalr x0,ra
 a0_eq_0:
     li      a0,-1 #pseudo to addi a0,x0,-1
     ret     #jalr x0,ra
-a1_eq_0:
+a1a2_eq_0:
     li      a0,-1 #pseudo to addi a0,x0,-1
 a3_le_0:
     ret     #jalr x0,ra
