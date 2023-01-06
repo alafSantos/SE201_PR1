708ddc8a9308c8e41c93f4b2ff78f24035a19e01
Alessandro Mandrile <mandrile@feedbackitalia.it>
2023-01-06 08:59:27 +0100

    + labels

--- program1.s
+++ program1.s
@@ -1,22 +1,28 @@
-addi    a7,a0,0x00
-addi    a0,a3,0x00
-beq     a7,zero,[0x40 + PC] 0x48
-beq     a1,zero,0x50
-beq     a2,zero,0x50
-bge     a3,zero,0x54
-addi    a5,a7,0x00
-slli    a4,a3,0x2
-add     a7,a7,a4
-lw      a4,0(a5)
-lw      a6,0(a1)
-add     a4,a4,a6
-sw      a4,0(a2)
-addi    a5,a5,4
-addi    a1,a1,4
-addi    a2,a2,4
-bne     a5,a7,0x24
-ret
-li      a0,-1
-ret
-li      a0,-1
-ret
+.text
+    addi    a7,a0,0x00
+    addi    a0,a3,0x00
+    beq     a7,zero, a0_eq_0 #[0x40 + PC] #0x48
+    beq     a1,zero, a1_eq_0 #0x50
+    beq     a2,zero, a2_eq_0 #0x50
+    bge     a3,zero, a3_le_0
+    addi    a5,a7,0x00
+    slli    a4,a3,0x02
+    add     a7,a7,a4
+if_5:
+    lw      a4,0(a5)
+    lw      a6,0(a1)
+    add     a4,a4,a6
+    sw      a4,0(a2)
+    addi    a5,a5,0x04
+    addi    a1,a1,0x04
+    addi    a2,a2,0x04
+a2_eq_0:
+    bne     a5,a7, if_5 #0x24
+    ret     #jalr x0,ra
+a0_eq_0:
+    li      a0,-1
+    ret     #jalr x0,ra
+a1_eq_0:
+    li      a0,-1
+a3_le_0:
+    ret     #jalr x0,ra
