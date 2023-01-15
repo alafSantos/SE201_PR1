09ec76a6138c288b50cfbc80b1c9aca49d2c58f8
alafSantos <alaf.ns4@gmail.com>
2023-01-15 11:39:58 +0100

    sum update

--- part4-sum-number.asm
+++ part4-sum-number.asm
@@ -1,18 +1,19 @@
 .text
 
 main:
-    mv a0, 0 //clean a0 register, expand to addi a0,zero,0
-
-    lib a0, 134 //since 134 is bigger than 127 we must use the unsigned version
-    mv a1, 0 // addi a1,zero,0
-    //expansion of mv a1, 65408 // 0xff80
-    lib a1, 0xff
+    mv a0, zero //clean a0 register, expand to add a0,zero,zero
+    li a0, 134 //since 134 is bigger than 127 we must use the unsigned version
+    
+    mv a1, zero // add a1,zero,zero
+    //expansion of li a1, 65408 // 0x0000ff80
+    li a1, 0xff 
     slli a1,8
-    lib a1, 0x80
-    //
+    //li a1, 0x80 // using the delay slot
+    
     call sum
-    lib a1, 0x80 // using the delay slot
+    li a1, 0x80 
 
 sum:
-   add a1,a1,a2
+   //add a0,a0,a1 using the delay slot
    jr lr  
+   add a0,a0,a1
